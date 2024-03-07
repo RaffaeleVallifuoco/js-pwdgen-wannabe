afbd45b45800f7df8d1964a40cc898c1d5e969c3
RaffaeleVallifuoco <ra.vallifuoco@outlook.it>
2024-03-07 12:36:41 +0100

    adding random number generator

--- script.js
+++ script.js
@@ -2,12 +2,13 @@ console.log('prova');
 
 const username = prompt('Inserisci il tuo nome'); //string ;
 const last_name = prompt('Inserisci il tuo cognome'); //string;
-const randomInt=1
+const randomInt=Math.floor((Math.random()*100)+1);
 
 
 console.log('nome:' + username);
 
 console.log('nome:' + last_name);
+
 
 
 prompt('Your Js password is : ' + username + last_name + randomInt);
