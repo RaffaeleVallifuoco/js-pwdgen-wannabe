e4c9fbab976eeb2a4c306b57e3f52587dd93833f
RaffaeleVallifuoco <ra.vallifuoco@outlook.it>
2024-03-07 12:32:58 +0100

    creating index & js

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log('prova');
+
+const username = prompt('Inserisci il tuo nome'); //string ;
+const last_name = prompt('Inserisci il tuo cognome'); //string;
+const randomInt=1
+
+
+console.log('nome:' + username);
+
+console.log('nome:' + last_name);
+
+
+prompt('Your Js password is : ' + username + last_name + randomInt);
+
