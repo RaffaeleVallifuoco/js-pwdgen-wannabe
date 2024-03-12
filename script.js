22033872b1d6505d4e2292e4bc4526fcd6eed529
RaffaeleVallifuoco <ra.vallifuoco@outlook.it>
2024-03-12 09:04:46 +0100

    code cleaning

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 console.log('prova');
 
-const username = prompt('Inserisci il tuo nome'); //string ;
-const last_name = prompt('Inserisci il tuo cognome'); //string;
-const randomInt=Math.floor((Math.random()*100)+1); //number
+const username = prompt('Inserisci il tuo nome'); //string  | null
+const last_name = prompt('Inserisci il tuo cognome'); //string | null
+const randomInt = Math.floor((Math.random() * 100) + 1); //number | null
 
 
 console.log('nome:' + username);
 
 console.log('nome:' + last_name);
 
+const password = username+last_name;
+console.log('pw : '+ password);
 
 
-prompt('Your Js password is : ' + username + last_name + randomInt);
+prompt('Your Js password is : ' + password + randomInt);
 
